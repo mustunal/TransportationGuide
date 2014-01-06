4d8384740f0d297688dab720c55f1b18beba2df6
mustunal <murat_ustunal@hotmail.com>
2014-01-06 06:59:06 -0800

    Proje Repository ye eklendi

---
+++ src/app/TransportationGuide.BusinessLogicLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportationGuide.BusinessLogicLayer
+{
+    public class Class1
+    {
+    }
+}
